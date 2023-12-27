166ef327ce3cf4a107ff5d16f36f0f5599005acf
Alex <57966024+Veymo-geek@users.noreply.github.com>
2023-12-27 18:34:58 +0200

    split audio function

--- test.py
+++ test.py
@@ -1,2 +1,22 @@
-print("Hello World!")
-print("Hew Feature!")
+import os
+from audio_separator.separator.separator import Separator
+
+files_folder = "Temp_files/"
+
+def split_audio(file_name):
+    file_path = files_folder + file_name
+    instrumental_output = files_folder + "Instrumental.flac"
+    vocal_output = files_folder + "Vocal.flac"
+
+    separator = Separator(
+        audio_file_path=file_path,
+        primary_stem_path=instrumental_output,
+        secondary_stem_path=vocal_output,
+        output_format="FLAC"
+    )
+
+    output_files = separator.separate()
+    print("Output files:", output_files)
+
+# Split the specified audio file
+split_audio("Audio.mp3")
