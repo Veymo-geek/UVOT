d9edfa00fd1a34b1d01d66ba59aaec42b1fe1a71
Alex <57966024+Veymo-geek@users.noreply.github.com>
2024-01-02 23:59:41 +0200

    Created main

---
+++ main.py
@@ -0,0 +1,14 @@
+from split_audio import split_audio
+
+split_audio("Audio.mp3")
+
+
+# get_video()
+# separate_video_audio_subs()
+# normaliaze_subs()
+# translate_text()
+# speech_synthesis()
+# combine_speech()
+# split_audio()
+# combine_audio()
+# combine_video()
